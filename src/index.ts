77103a4a96293b0287c20f616dc66037e20f3e77
Mauricio-Camilo <mauricio.ecamilo@gmail.com>
2022-07-07 18:52:11 -0500

    initializing project

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express, {json} from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(json());
+app.use(cors());
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}.`);
+});
